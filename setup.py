1112bcf9c26a6a72db09877e669a69e7d47ae28d
Yakuden <y.nursultanov@kazdream.kz>
2021-08-17 15:55:09 +0600

    update setup, LICENSE

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
-
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='telegaden',
-    version='0.0.1',
+    version='0.0.3',
     packages=find_packages(),
     url='https://github.com/Yakuden/telega',
     license='MIT',
@@ -15,5 +18,7 @@ setup(
         'telega': ['td_lib/linux/libtdjson.so', 'td_lib/linux/libtdjson.so.1.4.0'],
     },
     python_requires=">=3.5",
-    install_requires=[]
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
